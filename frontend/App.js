20c1cbc200c42476dfd0bda456b99554a7761025
hassannh <hassannouhi91@gmail.com>
2024-02-27 11:06:28 +0100

    add home and profile screen and navigate between theme

--- frontend/App.js
+++ frontend/App.js
@@ -1,13 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from 'react-redux';
+import store from './Redux/store'
+import navigator from './TabBarNavigation/index'
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+
+    // <Provider store={store}>
+      <NavigationContainer>
+
+        <Stack.Navigator>
+
+          <Stack.Screen name="AppTabs" component={navigator} />
+        
+        </Stack.Navigator>
+
+      </NavigationContainer>
+    // </Provider>
   );
+
+
+
 }
 
 const styles = StyleSheet.create({
